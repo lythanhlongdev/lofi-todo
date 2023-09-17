b673da98d34061fc4ca8491de90c13831e22786a
- <haiconbo2020@gmail.com>
2023-09-17 18:50:11 +0700

    add logic

--- fe/src/components/PopupFeature/PopupLogin.jsx
+++ fe/src/components/PopupFeature/PopupLogin.jsx
@@ -1,8 +1,28 @@
-import React from 'react'
-
+import React, { useState } from 'react'
+import login from '../../services/AuthService'
 import classes from '../../assets/css/PopupLogin.module.css'
 
 const PopupLogin = () => {
+    const [userInput, setUserInput] = useState({
+        username: "",
+        password: "",
+        captcha: ""
+    });
+
+    const inputChangeHandler = (event) => {
+        setUserInput((prevState) => {
+            return {
+                ...prevState,
+                [event.target.name]: event.target.value,
+            };
+        });
+    };
+
+    const onSubmitHandler = () => {
+        // login(userInput).then((res) => { console.log(res) })
+        console.log(userInput);
+    }
+
     return (
         <div className={classes.login}>
             <div className={classes['login_header']}>
@@ -10,18 +30,33 @@ const PopupLogin = () => {
             </div>
             <div className={classes['login_username']}>
                 <label for="username">Username (*)</label>
-                <input type="text" id='username' />
+                <input
+                    type="text"
+                    id='username'
+                    name="username"
+                    onChange={inputChangeHandler}
+                />
             </div>
             <div className={classes['login_password']}>
                 <label for="password">Password (*)</label>
-                <input type="password" id='password' />
+                <input
+                    type="password"
+                    id='password'
+                    name="password"
+                    onChange={inputChangeHandler}
+                />
             </div>
             <div className={classes['login_captcha']}>
-                <input type="text" id='captcha' />
+                <input
+                    type="text"
+                    id='captcha'
+                    name="captcha"
+                    onChange={inputChangeHandler}
+                />
                 <label for="captcha">captcha</label>
             </div>
             <div className={classes['login_btn']}>
-                <button>Login</button>
+                <button onClick={onSubmitHandler}>Login</button>
             </div>
             <div className={classes['login_forgot']}>
                 <p>Forgot password ?</p>
